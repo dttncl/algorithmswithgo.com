90ec13dd9d03336b2a4d97bdb8b5fe0048c9a5df
dttncl <82695034+dttncl@users.noreply.github.com>
2024-03-01 15:43:33 -0500

    updated soln BaseToDec

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,13 +1,25 @@
 package module01
 
+import (
+	"math"
+	"strings"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
 // Eg:
 //
-//   BaseToDec("E", 16) => 14
-//   BaseToDec("1110", 2) => 14
-//
+//	BaseToDec("E", 16) => 14
+//	BaseToDec("1110", 2) => 14
 func BaseToDec(value string, base int) int {
-	return 0
+	charset := "0123456789ABCDEF"
+	cpt := len(value) - 1
+	sum := 0
+	for _, val := range value {
+		num := strings.Index(charset, string(val))
+		sum += num * int(math.Pow(float64(base), float64(cpt)))
+		cpt--
+	}
+	return sum
 }
