ef43ce23c4f79f174a2d2f63af98bdc2d5482d42
dttncl <82695034+dttncl@users.noreply.github.com>
2024-03-01 11:45:10 -0500

    updated soln Sum

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,18 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+
+	//version1
+	//sum := 0
+	//for _, val := range numbers {
+	//	sum += val
+	//}
+	//return sum
+
+	//version2
+	if len(numbers) == 0 {
+		return 0 // if len of array is 0, return 0
+	}
+
+	return numbers[0] + Sum(numbers[1:]) // else, take first element and add with Sum(remaining elements)
 }
